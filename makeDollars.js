f2b7360c2157b3bf4d3c0ff2887399ac15843118
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-10-27 15:38:33 -0500

    test number

--- makeDollars.js
+++ makeDollars.js
@@ -2,7 +2,7 @@
  * Created by danesmith on 10/27/15.
  */
 var dollars = function(number){
-    var dollar = number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+    var dollar = Number(number).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
     return "$" + dollar;
 };
 
